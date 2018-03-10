ee4d562289d758641a7c9e299805e1e7ca542dda
Swanand Keskar <swanand.keskar@gmail.com>
2018-03-10 10:25:27 +0530

    karma config autowatch false

--- karma.conf.js
+++ karma.conf.js
@@ -26,7 +26,7 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
+    autoWatch: false,
     browsers: ['ChromeHeadless'],
      // you can define custom flags
      customLaunchers: {
